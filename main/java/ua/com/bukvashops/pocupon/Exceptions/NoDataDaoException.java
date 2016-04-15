de5b415132da8a1eee4fc9c4d8b00dc638650ec3
lomk <materynko@gmail.com>
2016-04-15 09:49:19 +0300

    1.1
    1.1

--- main/java/ua/com/bukvashops/pocupon/Exceptions/NoDataDaoException.java
+++
@@ -1,9 +0,0 @@
-package ua.com.bukvashops.pocupon.Exceptions;
-
-/**
- * Created by Igor on 22-Mar-16.
- */
-public class NoDataDaoException extends Throwable {
-    public NoDataDaoException(String s) {
-    }
-}
